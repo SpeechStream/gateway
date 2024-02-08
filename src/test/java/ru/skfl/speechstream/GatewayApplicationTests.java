b1df840238cc38f9c545b329cbff439cb44e2348
Eduard Rogachkov <edik.rogachkov@gmail.com>
2024-02-08 20:35:40 +0300

    Initial commit

---
+++ src/test/java/ru/skfl/speechstream/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skfl.speechstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
